452eed18093b88d0d2cc94090667df633e3d18d8
Antonio Bevilacqua <b3by.in.th3.sky@gmail.com>
2019-09-30 14:05:40 +0100

    source changed

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 
-setup(name='pymu',
+setup(name='pymudata',
       version='0.0.1',
       description='Wrapepr for inertial signal datasets',
       author='Antonio Bevilacqua',
       author_email='b3by.in.th3.sky@gmail.com',
       maintainer='Antonio Bevilacqua',
       maintainer_email='b3by.in.th3.sky@gmail.com',
-      download_url='https://github.com/b3by/pymu/archive/v0.0.1.tar.gz',
+      download_url='https://github.com/b3by/pymudata/archive/v0.0.1.tar.gz',
       packages=['pymu'],
       classifiers=[
           'Intended Audience :: Developers',
