e8f56bcc321f3960e732b23ded558f0f606f5d47
Antonio Bevilacqua <b3by.in.th3.sky@gmail.com>
2019-09-30 11:57:10 +0100

    setup completed

--- setup.py
+++ setup.py
@@ -8,9 +8,14 @@ setup(name='pymu',
       description='Wrapepr for inertial signal datasets',
       author='Antonio Bevilacqua',
       author_email='b3by.in.th3.sky@gmail.com',
+      download_url='https://github.com/b3by/pymu/archive/v0.0.1.tar.gz',
       packages=['pymu'],
       classifiers=[
           'Intended Audience :: Developers',
           'Topic :: Software Development :: Build Tools',
           'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.7'])
+          'Programming Language :: Python :: 3.7'],
+      install_requires=[
+          'numpy',
+          'pandas'
+      ])
